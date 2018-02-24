780f084408015732f93422f24d1a0c01d5c186e1
bimaldas1980 <bimaldas.bl@gmail.com>
2018-02-24 19:37:12 +1000

    commit changes

---
+++ src/app/Models/Address.ts
@@ -0,0 +1,18 @@
+export class Address {
+
+    public name: string;
+    public addressone: string;
+    public addresstwo: string;
+    public city: string;
+    public state: string;
+    public pincode: string;
+
+    
+    constructor() {
+
+    }
+
+
+
+    
+}
--- src/app/address-list/address-list.component.ts
+++ src/app/address-list/address-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Address } from '../Models/Address';
 
 @Component({
   selector: 'app-address-list',
@@ -7,7 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddressListComponent implements OnInit {
 
-  constructor() { }
+  addresses: Address[] = [];
+
+
+  constructor() { 
+    let addr = new Address();
+    addr.name = "Bimal";
+    addr.addressone = "U29";
+    addr.addresstwo = "42 Wattlebird St.";
+    addr.city = "Mango Hill";
+    addr.state = "QLD";
+    addr.pincode = "4509";
+    this.addresses.push(addr);
+
+    addr = new Address();
+    addr.name = "Doni";
+    addr.addressone = "U44";
+    addr.addresstwo = "42 Wattlebird St.";
+    addr.city = "Mango Hill";
+    addr.state = "QLD";
+    addr.pincode = "4509";
+    this.addresses.push(addr);
+
+    addr = new Address();
+    addr.name = "Juby";
+    addr.addressone = "U46";
+    addr.addresstwo = "42 Wattlebird St.";
+    addr.city = "Mango Hill";
+    addr.state = "QLD";
+    addr.pincode = "4509";
+    this.addresses.push(addr);
+  }
 
   ngOnInit() {
   }
